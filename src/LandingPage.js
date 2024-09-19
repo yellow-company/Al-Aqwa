729486e28e628948a4a049e46d16131d10021a82
muhammedhussam <muhamed.hussam.12@gmail.com>
2024-09-19 10:22:17 +0300

    new upates for mobile view

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./LandingPage.css";
 import logo from "../src/assets/logo.png";
 import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
+import Icon from "@mdi/react";
+import { mdiPhone, mdiStar } from "@mdi/js";
 
 const LandingPage = () => {
   return (
@@ -34,7 +36,7 @@ const LandingPage = () => {
           >
             Services
           </button>
-          <button
+          {/* <button
             onClick={() => {
               const element = document.getElementById("testimonials");
               const yOffset = -50; // Adjust this value to match the height of the top bar
@@ -45,7 +47,7 @@ const LandingPage = () => {
             }}
           >
             Testimonials
-          </button>
+          </button> */}
           <button
             onClick={() => {
               const element = document.getElementById("about");
@@ -115,7 +117,7 @@ const LandingPage = () => {
         <div className="section-service-image cloud-box cloud-box-image"></div>
       </section>
 
-      <section id="testimonials" className="landing-page-section">
+      {/* <section id="testimonials" className="landing-page-section">
         <div className="section-testimonials-image cloud-box cloud-box-image"></div>
         <div className="cloud-box-content ">
           <h3>What Our Clients Say</h3>
@@ -151,12 +153,46 @@ const LandingPage = () => {
             <br />
           </ul>
         </div>
-      </section>
+      </section> */}
 
       <section id="about" className="landing-page-section">
         <div className="cloud-box-content ">
           <h3>About Us</h3>{" "}
           <p>We are a top-quality air conditioning service provider...</p>
+          <div className="cloud-box-content ">
+            <h3>What Our Clients Say</h3>
+            <ul>
+              <li>
+                "CoolAir Solutions provided excellent service and the new unit
+                works perfectly!"<b> - John Do.</b>
+              </li>
+              <br />
+              <br />
+              <li>
+                "Their maintenance service saved us a lot of money in the long
+                run."
+                <b> - Ziad Ahmed </b>
+              </li>
+
+              <br />
+              <br />
+              <li>
+                "CoolAir Solutions provided excellent service and the new unit
+                works perfectly!" <b>- John Do.</b>
+              </li>
+              <br />
+              <br />
+
+              <li>
+                "Their maintenance service saved us a lot of money in the long
+                run."
+                <b> - Sarah Ko. </b>
+              </li>
+
+              <br />
+              <br />
+            </ul>
+          </div>
         </div>
         <div className="section-about-image  cloud-box cloud-box-image"></div>
       </section>
@@ -171,42 +207,125 @@ const LandingPage = () => {
           <div className="contact-section">
             <h3>Contact Us</h3>
             <ul className="phone-numbers-list">
-              <li>Phone: (123) 456-7890</li>
-              <li>Phone: (987) 654-3210</li>
-              <li>Phone: (555) 123-4567</li>
+              <li>
+                {/* <Icon
+                  path={mdiStar}
+                  size={1}
+                  color="#000"
+                  // style={{ marginLeft: "5px" }}
+                  // className="phone-icon"
+                /> */}
+                {/* <span className="custom-bullet">⭐</span> */}
+                (+20) 1120747503
+                <a
+                  href="tel:+201120747503"
+                  className="phone-button"
+                  // aria-label="Call (+2) 01120747503"
+                >
+                  <Icon
+                    path={mdiPhone}
+                    size={1.2}
+                    color="#17b33e"
+                    // style={{ marginLeft: "5px" }}
+                    className="phone-icon"
+                  />
+                </a>
+              </li>
+              <li>
+                {/* <Icon
+                  path={mdiPhone}
+                  size={1.2}
+                  color="#17b33e"
+                  // style={{ marginLeft: "5px" }}
+                  // className="phone-icon"
+                /> */}
+                {/* <span className="custom-bullet">⭐</span> */}
+                (+20) 1070824128
+                <a
+                  href="tel:+201070824128"
+                  className="phone-button"
+                  // aria-label="Call (+2) 01070824128"
+                >
+                  <Icon
+                    path={mdiPhone}
+                    size={1.2}
+                    color="#17b33e"
+                    // style={{ marginLeft: "5px" }}
+                    className="phone-icon"
+                  />
+                </a>
+              </li>
+              <li>
+                {/* <Icon
+                  path={mdiPhone}
+                  size={1.2}
+                  color="#17b33e"
+                  // style={{ marginLeft: "5px" }}
+                  // className="phone-icon"
+                /> */}
+                {/* <span className="custom-bullet">⭐</span> */}
+                (+20) 1126165936
+                <a
+                  href="tel:+201126165936"
+                  className="phone-button"
+                  // aria-label="Call (+2) 01126165936"
+                >
+                  <Icon
+                    path={mdiPhone}
+                    size={1.2}
+                    color="#17b33e"
+                    // style={{ marginLeft: "5px" }}
+                    className="phone-icon"
+                  />
+                </a>
+              </li>
             </ul>
           </div>
 
-          <p>Email: info@coolairsolutions.com</p>
-          <div className="social-media-icons">
-            <p>Social Media:</p>
-            <a
-              href="https://www.facebook.com/profile.php?id=61563846054491&mibextid=LQQJ4d"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button>
-                <FaFacebookF />
-              </button>
-            </a>
-            <a
-              href="https://www.instagram.com/alaqwa_?igsh=MWhrNjc3aWM3bWF1eQ%3D%3D&utm_source=qr"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button>
-                <FaInstagram />
-              </button>
-            </a>
-            <a
-              href="https://www.tiktok.com/@alaqwa_1?_t=8p5yo1b5MkE&_r=1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button>
-                <FaTiktok />
-              </button>
-            </a>
+          {/* <p>Email: info@coolairsolutions.com</p> */}
+          <div className="social-media-icons-container">
+            <div className="follow-box">
+              <p>Follow Us</p>
+            </div>
+
+            <div className="icon-container">
+              <div className="fb-icon">
+                <a
+                  href="https://www.facebook.com/profile.php?id=61563846054491&mibextid=LQQJ4d"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="fb-icon"
+                >
+                  <button>
+                    <FaFacebookF />
+                  </button>
+                </a>
+              </div>
+              <div className="ig-icon">
+                <a
+                  href="https://www.instagram.com/alaqwa_?igsh=MWhrNjc3aWM3bWF1eQ%3D%3D&utm_source=qr"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ig-icon"
+                >
+                  <button>
+                    <FaInstagram size={"32px"} />
+                  </button>
+                </a>
+              </div>
+              <div className="tiktok-icon">
+                <a
+                  href="https://www.tiktok.com/@alaqwa_1?_t=8p5yo1b5MkE&_r=1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="tiktok-icon"
+                >
+                  <button>
+                    <FaTiktok />
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
